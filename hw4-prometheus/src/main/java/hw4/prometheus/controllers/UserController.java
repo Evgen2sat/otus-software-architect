106b55c0f4553685e48dde8f841580034d607dcf
Evgeniy <lev-sait@mail.ru>
2020-04-30 17:07:52 +0300

    включена поддержка метрик

--- hw4-prometheus/src/main/java/hw4/prometheus/controllers/UserController.java
+++ hw4-prometheus/src/main/java/hw4/prometheus/controllers/UserController.java
@@ -2,6 +2,9 @@ package hw4.prometheus.controllers;
 
 import hw4.prometheus.dto.UserDto;
 import hw4.prometheus.repositories.UserRepository;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
 
@@ -11,28 +14,87 @@ import java.sql.SQLException;
 @Controller("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+//@Timed(value = "app_request_latency", extraTags = {"test1", "test2"}, histogram = true, description = "Application Request Latency")
 public class UserController {
+
+    private MeterRegistry meterRegistry;
+
+    public UserController(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
 
     @Inject
     private UserRepository userRepository;
 
     @Post
     public UserDto create(@Body UserDto user) throws SQLException {
-        return userRepository.create(user);
+        return Timer.builder("app_request_latency")
+                .description("Application Request Latency")
+                .tag("method", "POST")
+                .tag("endpoint", "/users")
+                .publishPercentileHistogram()
+                .register(this.meterRegistry)
+                .record(() -> {
+                    try {
+                        return userRepository.create(user);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+//        return userRepository.create(user);
     }
 
     @Put("/{id}")
     public UserDto create(long id, @Body UserDto user) throws SQLException {
-        return userRepository.update(id, user);
+        return Timer.builder("app_request_latency")
+                .description("Application Request Latency")
+                .tag("method", "UPDATE")
+                .tag("endpoint", "/" + id)
+                .publishPercentileHistogram()
+                .register(this.meterRegistry)
+                .record(() -> {
+                    try {
+                        return userRepository.update(id, user);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+//        return userRepository.update(id, user);
     }
 
     @Get("/{id}")
     public UserDto get(long id) throws SQLException {
-        return userRepository.get(id);
+        return Timer.builder("app_request_latency")
+                .description("Application Request Latency")
+                .tag("method", "GET")
+                .tag("endpoint", "/" + id)
+                .publishPercentileHistogram()
+                .register(this.meterRegistry)
+                .record(() -> {
+                    try {
+                        return userRepository.get(id);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+//        return userRepository.get(id);
     }
 
     @Delete("/{id}")
     public void delete(long id) throws SQLException {
-        userRepository.delete(id);
+        Timer.builder("app_request_latency")
+                .description("Application Request Latency")
+                .tag("method", "DELETE")
+                .tag("endpoint", "/" + id)
+                .publishPercentileHistogram()
+                .register(this.meterRegistry)
+                .record(() -> {
+                    try {
+                        userRepository.delete(id);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+//        userRepository.delete(id);
     }
 }
