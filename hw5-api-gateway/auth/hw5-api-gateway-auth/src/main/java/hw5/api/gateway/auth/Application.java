b92049159e3e316fa1e476470db932e240edaa5d
Evgeniy <lev-sait@mail.ru>
2020-05-16 11:26:24 +0300

    добавлен проект аутентификации

---
+++ hw5-api-gateway/auth/hw5-api-gateway-auth/src/main/java/hw5/api/gateway/auth/Application.java
@@ -0,0 +1,10 @@
+package hw5.api.gateway.auth;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
