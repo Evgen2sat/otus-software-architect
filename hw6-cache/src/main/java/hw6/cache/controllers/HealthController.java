cec580a005f5a3df2db91b2883670a12c3217dfe
Evgeniy <lev-sait@mail.ru>
2020-06-06 13:24:12 +0300

    Добавлен кэш

--- hw6-cache/src/main/java/hw6/cache/controllers/HealthController.java
+++ hw6-cache/src/main/java/hw6/cache/controllers/HealthController.java
@@ -1,10 +1,8 @@
 package hw6.cache.controllers;
 
 import hw6.cache.dto.HealthDto;
-import io.micronaut.http.HttpHeaders;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -14,8 +12,9 @@ import io.micronaut.http.annotation.Produces;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class HealthController {
+
     @Get
-    public HttpResponse auth(final HttpHeaders headers) {
+    public HttpResponse health() {
         HealthDto healthDto = new HealthDto();
         healthDto.setStatus("OK");
         return HttpResponse.ok(healthDto);
--- hw6-cache/src/main/java/hw6/cache/services/ProductService.java
+++ hw6-cache/src/main/java/hw6/cache/services/ProductService.java
@@ -1,16 +1,33 @@
 package hw6.cache.services;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import hw6.cache.dto.ProductDto;
 import hw6.cache.repositories.ProductRepository;
 import io.micronaut.http.HttpResponse;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.lang.reflect.Type;
 import java.sql.SQLException;
 import java.util.List;
 
 @Singleton
 public class ProductService {
+
+    private Jedis jedis;
+    private Type itemsListType;
+
+    public ProductService() {
+        if (Integer.valueOf(System.getenv("CACHE_ENABLED")) == 1) {
+            this.jedis = new Jedis(System.getenv("REDIS_HOST"), Integer.valueOf(System.getenv("REDIS_PORT")));
+            this.itemsListType = new TypeToken<List<ProductDto>>() {
+            }.getType();
+        }
+    }
+
     @Inject
     private ProductRepository productRepository;
 
@@ -18,7 +35,7 @@ public class ProductService {
         ProductDto productDto = null;
         try {
             productDto = productRepository.getProduct(id);
-            if(productDto == null) {
+            if (productDto == null) {
                 return HttpResponse.badRequest("product not found");
             }
         } catch (SQLException e) {
@@ -62,12 +79,67 @@ public class ProductService {
 
     public HttpResponse getProducts(String name, String color, Float price) {
         List<ProductDto> products = null;
+
+        if (jedis != null) {
+            //проверим в кэше
+            products = getProductsFromCache(name, color, price);
+
+            if (products != null && !products.isEmpty()) {
+                return HttpResponse.ok(products);
+            }
+        }
+
         try {
             products = productRepository.getProducts(name, color, price);
+
+            addProductsToCache(products, name, color, price);
         } catch (SQLException e) {
             return HttpResponse.serverError(e);
         }
 
         return HttpResponse.ok(products);
+    }
+
+    private List<ProductDto> getProductsFromCache(String name, String color, Float price) {
+        String cacheValue = jedis.get(getCacheKey(name, color, price));
+
+        return new Gson().fromJson(cacheValue, itemsListType);
+    }
+
+    private void addProductsToCache(List<ProductDto> products, String name, String color, Float price) {
+        //если есть результаты
+        if (products != null && !products.isEmpty()) {
+            //то добавим в кэш
+            String cacheValue = new Gson().toJson(products);
+            String cacheKey = getCacheKey(name, color, price);
+            jedis.set(cacheKey, cacheValue, new SetParams().ex(300));
+        }
+    }
+
+    private String getCacheKey(String name, String color, Float price) {
+        StringBuilder key = new StringBuilder();
+
+        if (name != null && !name.isEmpty()) {
+            key.append("#")
+                    .append("name:")
+                    .append(name)
+                    .append("#");
+        }
+
+        if (color != null && !color.isEmpty()) {
+            key.append("#")
+                    .append("color:")
+                    .append(color)
+                    .append("#");
+        }
+
+        if (price != null) {
+            key.append("#")
+                    .append("price:")
+                    .append(price)
+                    .append("#");
+        }
+
+        return key.toString();
     }
 }
